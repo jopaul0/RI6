7a2c2e7557f0bb734e33dac2de5ff9c1a7acfa1b
João Paulo <jopaulo.as8@gmail.com>
2026-03-04 20:22:57 -0300

    chore: ajuste na Radicacao

--- src/models/Radiciacao.ts
+++ src/models/Radiciacao.ts
@@ -1,7 +1,7 @@
 import Operacao from "./Operacao";
 
 export default class Radiacao extends Operacao {
-    public calcular(x: number): number {
-        return x ** 0.5;
+    public calcular(a: number, b : number = 2): number {
+        return a ** (1 / b);
     }
 }
